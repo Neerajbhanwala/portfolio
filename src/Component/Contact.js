8d3cba43ae053ee4a96ce7f2c2e4819476b186be
kukaa <kukaa904@gmail.com>
2022-12-07 18:38:12 +0530

    first commit

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -2,18 +2,49 @@ import React from 'react'
 import '../Style/Contact.css';
 
 function Contact() {
+  function myFunction() {
+    var a = prompt("Enter your name");
+    var b = prompt("Enter your email");
+    var c = prompt("Enter any message");
+    let x = document.getElementById("fname");
+    let y = document.getElementById("email");
+    let z = document.getElementById("textarea");
+    alert("Are you sure that your name is " +a+ " / Your email id is " +b+ " / Your message is "+c)
+    x.value = (a);
+    y.value = (b);
+    z.value = (c);
+  }
+  function clk(){
+    let x = document.getElementById("fname");
+    let y = document.getElementById("email");
+    let z = document.getElementById("textarea");
+    if(x.value===""){
+    alert("Please provide input firstly");
+    }
+    else{
+    x.value = ("");
+    y.value = ("");
+    z.value = ("");
+    alert("Data Saved successfully");
+  }
+  }
   return (
     <>
      <div id="Contact" className='mains p-5'>
         <span className='ms-5 text-light' style={{fontSize:"35px"}}>Contact me</span><br /> 
         <span className='ms-5' style={{fontSize:"50px", fontFamily:"cursive", color:"#b7c5d3"}}>Get In Touch</span><br />   
-        <input type="text" placeholder="Name" />
+        <input  onClick={myFunction} id="fname" type="text" placeholder="Name" />
         <a href="https://accounts.google.com/" target="_"><div className="mail"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp; kukaa904@gmail.com</div></a>
-        <input type="text" placeholder="Email" />
+        <input id="email" type="text" placeholder="Email" />
         <a href="https://neeraj-bhanwala-kukaa.business.site/#details" target="_"><div className="mail"><i class="fa fa-phone" aria-hidden="true"></i> +91-99966-89916</div></a>
-        <textarea placeholder="Message"></textarea><br />
-        <button>Submit</button>
+        <textarea id="textarea" placeholder="Message"></textarea><br />
+        <button onClick={clk}>Submit</button>
     </div> 
+    <div className=' mt-2 mb-3 text-center'>
+      <h2>John Alex</h2>
+      <h6>NeerajBhanwala@All CopyRights Reserved 2022</h6>
+    
+    </div>
     </>
   )
 }
--- src/Component/HireMe.js
+++ src/Component/HireMe.js
@@ -8,7 +8,7 @@ function HireMe() {
       <div className='main p-5'>
       <span className='ms-5' style={{fontSize:"35px"}}>Hire me</span><br />
         <span className='ms-5' style={{fontSize:"50px", fontFamily:"cursive", color:"#b7c5d3"}}>For your projects</span><br />
-        <div style={{height:"405px", marginLeft:"110px", float:"left", width:"450px"}}><img src={person3} alt="person"  style={{height:"100%", width:"100%"}} /></div>
+        <div style={{height:"405px", marginLeft:"110px", float:"left", width:"400px"}}><img src={person3} alt="person"  style={{height:"100%", width:"100%"}} /></div>
         <div className='box'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean<br />
         <button>Hire me</button>
         </div>
