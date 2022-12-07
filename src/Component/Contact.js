b029d9d6c8a0519ef2c812d54c7654f60e61529e
kukaa <kukaa904@gmail.com>
2022-12-07 18:46:33 +0530

    first commit

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -18,7 +18,7 @@ function Contact() {
     let x = document.getElementById("fname");
     let y = document.getElementById("email");
     let z = document.getElementById("textarea");
-    if(x.value===""){
+    if(x,y,z.value===""){
     alert("Please provide input firstly");
     }
     else{
@@ -35,9 +35,9 @@ function Contact() {
         <span className='ms-5' style={{fontSize:"50px", fontFamily:"cursive", color:"#b7c5d3"}}>Get In Touch</span><br />   
         <input  onClick={myFunction} id="fname" type="text" placeholder="Name" />
         <a href="https://accounts.google.com/" target="_"><div className="mail"><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp; kukaa904@gmail.com</div></a>
-        <input id="email" type="text" placeholder="Email" />
+        <input id="email" type="text" disabled placeholder="Email" />
         <a href="https://neeraj-bhanwala-kukaa.business.site/#details" target="_"><div className="mail"><i class="fa fa-phone" aria-hidden="true"></i> +91-99966-89916</div></a>
-        <textarea id="textarea" placeholder="Message"></textarea><br />
+        <textarea id="textarea" disabled placeholder="Message"></textarea><br />
         <button onClick={clk}>Submit</button>
     </div> 
     <div className=' mt-2 mb-3 text-center'>
